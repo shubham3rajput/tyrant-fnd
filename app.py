ea82e4aef23c1afebab385295d2df81acbe707d9
shubham3rajput <skr010561@gmail.com>
2021-04-28 22:21:18 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request, render_template
+from predictionModel import PredictionModel
+import pandas as pd
+from random import randrange
+
+app = Flask(__name__, static_folder="./public/static",
+            template_folder="./public")
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    model = PredictionModel(request.json)
+    return jsonify(model.predict())
+
+
+@app.route('/random', methods=['GET'])
+def random():
+    data = pd.read_csv("data/fake_or_real_news_test.csv")
+    index = randrange(0, len(data)-1, 1)
+    return jsonify({'title': data.loc[index].title, 'text': data.loc[index].text})
+
+
+@app.route("/notebook")
+def notebook():
+    return render_template('Notebook.html')
+
+
+# Only for local running
+if __name__ == '__main__':
+    app.run()
+
+
+
+
---
+++ webapp/src/index.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Pipeline } from './pipeline'
+import './index.scss';
+class NLPInterface extends React.Component {
+
+  MIN_WORDS = 25;
+  MAX_WORDS = 3500;
+
+  state = {
+    text: '',
+    wordsHint: `You have to write at least ${this.MIN_WORDS} words`,
+    loading: false,
+    error: false,
+    prediction: null,
+    original: null,
+    pos_tagged: null,
+    preprocessed: null,
+    emoji: true
+  }
+
+
+  random = () => fetch("/random")
+    .then(response => response.json())
+    .then(article => article.title + '\n\n' + article.text)
+    .then(text => this.setState({
+      text, wordsHint: this.getWordsHint(text), prediction: null,
+      original: null,
+      pos_tagged: null,
+      preprocessed: null,
+      emoji: false
+    }))
+    .catch(error => this.setState({ error: true, emoji: false }));
+
+  updateInput = ({ target: { value } }) => this.setState({ text: value, prediction: null, wordsHint: this.getWordsHint(value) });
+
+  getWordsHint = (text) => {
+    let count = text.split(/\s/).length;
+    if (count < this.MIN_WORDS) return `You have to write ${this.MIN_WORDS - count} more words`
+    if (count >= this.MIN_WORDS) return `You can write up to ${this.MAX_WORDS - count} more words`
+  }
+  predict = () => {
+    if (this.MAX_WORDS - this.state.text.split(/\s/).length < 0) return;
+
+    this.setState({ loading: true, prediction: null });
+
+    fetch("/predict", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state.text)
+    })
+      .then(response => response.json())
+      .then(({ original, pos_tagged, preprocessed, prediction }) => {
+        this.setState({ loading: false, prediction, original, pos_tagged, preprocessed })
+      })
+      .catch(error => this.setState({ loading: false, error: true }))
+
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1> Fake News Detector</h1>
+        <textarea autoFocus disabled={this.state.loading} value={this.state.text} onChange={this.updateInput}>
+        </textarea>
+
+        <span className="hint"> {this.state.wordsHint}</span>
+
+        <button disabled={this.state.loading} className="random" onClick={this.random}>
+          Load random News from test dataset ? <span>Click here</span>.
+          <span role="img" className="emoji bounce">
+            {this.state.emoji ? "👆🏼" : ""}
+          </span>
+        </button>
+        <button disabled={
+          this.state.loading ||
+          this.MIN_WORDS - this.state.text.split(/\s/).length > 0 ||
+          this.MAX_WORDS - this.state.text.split(/\s/).length < 0} className="cta" onClick={this.predict}> Predict </button>
+
+        {this.state.loading ? <h1>Classifying ...</h1> : ''}
+
+        {this.state.error ? <h1>ERROR</h1> : ''}
+
+        <h1 className={this.state.prediction}>
+          {this.state.prediction}
+        </h1>
+
+        <Pipeline
+          prediction={this.state.prediction}
+          original={this.state.original}
+          pos_tagged={this.state.pos_tagged}
+          preprocessed={this.state.preprocessed}
+        />
+      </div>
+    )
+  }
+}
+
+
+
+ReactDOM.render(
+  <NLPInterface />,
+  document.getElementById('root'));
